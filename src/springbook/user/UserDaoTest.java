5493bd51988699ed1dc7c568ffc0aa51d09eb30c
2h5i <dev.2h5i@gmail.com>
2022-10-20 23:15:46 +0900

    factory configuration 등록까지, 책 97p

--- src/springbook/user/UserDaoTest.java
+++ src/springbook/user/UserDaoTest.java
@@ -1,14 +1,21 @@
-package springbook;
+package springbook.user;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springbook.user.dao.ConnectionMaker;
+import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
-public class SpringBookApplication {
+public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        UserDao dao = new UserDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
---
+++ src/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,26 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(new DConnectionMaker());
+    }
+
+    public AccountDao accountDao() {
+        return new AccountDao(new DConnectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(new DConnectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+}
